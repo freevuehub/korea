926c45e0392cde7b5925ca79242c79c017950799
freevue <hackish@hanmail.net>
2019-08-20 22:45:49 +0900

    [레이아웃] 풀이미지 헤더 적용 준비

---
+++ components/ImageHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    
+  </header>
+</template>
+
+<script lang="ts">
+  import { Vue, Component } from 'vue-property-decorator';
+
+
+  @Component
+  export default class ImageHeader extends Vue {
+    
+  }
+</script>
