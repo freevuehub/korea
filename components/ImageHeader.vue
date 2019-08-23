e6e74f32f83669624b5c2cdf73e7eb39035f9a46
freevue <hackish@hanmail.net>
2019-08-23 22:16:34 +0900

    [레이아웃] 이미지 헤더 제작

--- components/ImageHeader.vue
+++ components/ImageHeader.vue
@@ -1,15 +1,20 @@
 <template>
   <header>
-    
+    <img :src="img" alt="">
   </header>
 </template>
 
 <script lang="ts">
   import { Vue, Component } from 'vue-property-decorator';
 
-
-  @Component
+  @Component({
+    props: {
+      img: String
+    }
+  })
   export default class ImageHeader extends Vue {
-    
+    mounted() {
+      console.log(this);
+    }
   }
 </script>
