8be11e1edd39db46c41e0464e6b890270c13b1dc
홍성준 <freevue@hongseongjun-ui-MacBookPro.local>
2020-08-16 21:40:06 +0900

    [new]: 프로젝트 생성

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <nuxt />
+    </v-main>
+    <v-footer absolute app class="pa-3">
+      <strong class="mr-3">FreeVue</strong>
+      Copyright &copy; {{ state.year }}.
+    </v-footer>
+  </v-app>
+</template>
+
+<script lang="ts">
+import 'vuetify/dist/vuetify.min.css'
+
+import { defineComponent, reactive } from '@vue/composition-api'
+import dayjs from 'dayjs'
+
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      year: dayjs().year(),
+    })
+
+    return {
+      state,
+    }
+  },
+})
+</script>
