bc8b542026cd89feb5b3345e644ac29c7357da0b
홍성준 <freevue@hongseongjun-ui-MacBookPro.local>
2020-08-17 17:54:50 +0900

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,8 +17,8 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s - ' + 'FreeVue',
+    title: 'Korea',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
