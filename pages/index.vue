a43d6c776c644b81c2437e741473af210dbbb69b
freevue <hackish@hanmail.net>
2020-08-24 22:15:10 +0900

    [improved]: 등록된 유공자가 없는 경우 대응

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <h2 class="mb-2">오늘 등록된 유공자</h2>
-    <v-row justify="space-between">
+    <v-row v-if="!!computed.todayAddList.length" justify="space-between">
       <v-col v-for="person in computed.todayAddList" :key="person.id" cols="6">
         <v-card nuxt :to="`/person/${person.id}`">
           <v-img class="white--text align-end" height="200px" :src="person.imgUrl">
@@ -16,25 +16,7 @@
         </v-card>
       </v-col>
     </v-row>
-    <!-- <v-card
-      v-for="person in computed.personList"
-      :key="person.id"
-      class="d-flex mb-3"
-      nuxt
-      :to="`/person/${person.id}`"
-    >
-      <v-list-item three-line>
-        <v-list-item-content>
-          <div class="overline mb-4">{{ person.hunkuk }} / {{ person.work }}</div>
-          <v-list-item-title class="headline mb-1">{{ person.name }}</v-list-item-title>
-          <v-list-item-subtitle>{{ person.birthDay }} ~ {{ person.deathDay }}</v-list-item-subtitle>
-        </v-list-item-content>
-
-        <v-list-item-avatar tile size="80" color="grey">
-          <img :src="person.imgUrl || './noneImg.png'" :alt="person.name" />
-        </v-list-item-avatar>
-      </v-list-item>
-    </v-card>-->
+    <v-alert v-else type="info">오늘 등록된 유공자가 없습니다.</v-alert>
   </v-container>
 </template>
 
