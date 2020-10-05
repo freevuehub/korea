0c45e02d1101582f99aa7cb86b9382c49915a134
freevue <hackish@hanmail.net>
2020-10-05 16:06:28 +0900

    [improved]: 훈격 리스트 한번만 불러오게 수정

--- middleware/person-list.ts
+++ middleware/person-list.ts
@@ -3,7 +3,13 @@ import { getHunkukList } from '~/API'
 import { HunkukConst } from '~/Constant'
 
 export default async (context: Context) => {
-  const { result } = await getHunkukList()
+  const hunkukList = context.app.store?.getters[`hunkuk/${HunkukConst.$Get.List}`]
 
-  await context.app.store?.dispatch(`hunkuk/${HunkukConst.$Call.List}`, result)
+  if (!hunkukList.length) {
+    const { result } = await getHunkukList()
+
+    await context.app.store?.dispatch(`hunkuk/${HunkukConst.$Call.List}`, result)
+  }
+
+  console.log(context)
 }
