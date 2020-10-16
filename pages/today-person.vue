9b605e7bfc9bdb6bdb32589eed0d93ddda2a6ea5
freevue <hackish@hanmail.net>
2020-10-16 21:42:28 +0900

    [new]: 오늘의 인물 리스트를 보여줄 페이지 추가

---
+++ pages/today-person.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container>
+    <v-row class="justify-center">
+      <v-col sm="10">
+        <v-row>
+          <v-col sm="6" lg="4">
+            <person-image :url="todayPerson.imgUrl" />
+          </v-col>
+          <v-col sm="6" lg="8">
+            <person-info :item="todayPerson" />
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col sm="10">
+        <h2 class="mb-2">오늘 등록된 유공자</h2>
+        <add-list v-if="!!todayAddList.length" :list="todayAddList" />
+        <v-alert v-else :class="$round" elevation="10" type="info">
+          오늘 등록된 유공자가 없습니다.
+        </v-alert>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import { useState, useComputed } from './index.fn'
+import { TodayAddList, TodayPersonImgCard, TodayPersonInfoCard } from '~/components'
+
+export default defineComponent({
+  middleware: ['main'],
+  layout: 'main',
+  components: {
+    personImage: TodayPersonImgCard,
+    personInfo: TodayPersonInfoCard,
+    addList: TodayAddList,
+  },
+  setup(_, context) {
+    const state = useState()
+    const computed = useComputed(context)
+
+    return {
+      state,
+      ...computed,
+    }
+  },
+})
+</script>
