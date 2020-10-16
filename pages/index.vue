56548e0165d5283a6769bda04de2ed6bf6b0bfdc
freevue <hackish@hanmail.net>
2020-10-16 21:55:45 +0900

    [UX/UI]: 메인 페이지 구성 수정

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
-    <v-row class="justify-center flex-column">
-      <v-col xs="12" sm="10" md="10">
+    <v-row class="align-center flex-column">
+      <v-col xs="12" sm="10">
         <v-row>
           <v-col xs="12" sm="6" lg="4">
             <person-image :url="todayPerson.imgUrl" />
@@ -11,7 +11,7 @@
           </v-col>
         </v-row>
       </v-col>
-      <v-col xs="12" sm="10" md="10">
+      <v-col xs="12" sm="10">
         <h2 class="mb-2">오늘 등록된 유공자</h2>
         <add-list v-if="!!todayAddList.length" :list="todayAddList" />
         <v-alert v-else :class="$round" elevation="10" type="info">
@@ -29,7 +29,6 @@ import { TodayAddList, TodayPersonImgCard, TodayPersonInfoCard } from '~/compone
 
 export default defineComponent({
   middleware: ['main'],
-  layout: 'main',
   components: {
     personImage: TodayPersonImgCard,
     personInfo: TodayPersonInfoCard,
