e7db0e5fccc0464edb4101ec99d09a9cc8850df6
freevue <hackish@hanmail.net>
2020-10-26 21:38:59 +0900

    [new]: 사건을 보여줄 페이지 추가

---
+++ pages/history-event.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container fluid>
+    history-event
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
