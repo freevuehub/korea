eef3a4f0497347fda69a4896da5f45aa0da47d65
freevue <hackish@hanmail.net>
2020-12-01 21:48:37 +0900

    [UX/UI]: 메인 구성 방법 수정

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,29 @@
 <template>
   <v-container fluid>
-    <face-grid v-if="!isMobile" />
     <v-row class="align-center flex-column pa-0">
-      <v-col xs="12" sm="10" md="10" lg="8" class="py-0">
-        <v-row ref="today-person" class="flex-column align-end person-info py-0">
-          <v-col v-if="!isMobile" xs="12">
-            <h1 class="mt-10 mb-5 text-center">오늘의 인물</h1>
-          </v-col>
-          <v-col
-            xs="12"
-            :sm="state.todayPersonImageView ? '4' : '12'"
-            class="thumb"
-            :class="{ on: state.todayPersonImageView, 'mb-10': !isMobile }"
-          >
+      <v-col xs="12" sm="10" md="10" lg="8" class="py-0 mb-5">
+        <v-row ref="today-person" class="align-start person-info py-0">
+          <v-col xs="12" sm="6" class="thumb" :class="{ pc: !isMobile }">
             <person-image :url="todayPerson.imgUrl" />
           </v-col>
-          <v-col xs="12" sm="8" class="information" :class="{ 'mb-5': !isMobile }">
+          <v-col xs="12" sm="6" class="information">
             <person-info :item="todayPerson" />
-          </v-col>
-          <v-col v-if="!isMobile" xs="12" sm="8">
-            <v-card :class="$round" elevation="10">
+            <v-card v-if="!isMobile" :class="$round" elevation="10">
               <achivement :achivement="todayPerson.achivement" />
             </v-card>
           </v-col>
         </v-row>
       </v-col>
-      <v-col xs="12" sm="10" md="10" lg="8">
-        <h1 :class="{ 'mt-10': !isMobile, 'mb-5': !isMobile, textCenter: !isMobile }">
-          오늘 등록된 유공자
-        </h1>
-        <v-alert v-if="!todayAddList.length" :class="$round" elevation="10" type="info">
-          오늘 등록된 유공자가 없습니다.
-        </v-alert>
-        <v-row v-else justify="space-between">
-          <v-col
-            v-for="person in todayAddList"
-            :key="person.id"
-            cols="12"
-            xs="12"
-            sm="12"
-            md="6"
-            class="py-0"
-          >
-            <person class="mb-5" :item="person" />
+      <v-col xs="12" sm="10" md="10" lg="8" class="py-0">
+        <v-row ref="today-person" class="align-start person-info py-0">
+          <v-col xs="12" sm="6" class="thumb" :class="{ pc: !isMobile }">
+            <person-image :url="todayPerson.imgUrl" />
+          </v-col>
+          <v-col xs="12" sm="6" class="information">
+            <person-info :item="todayPerson" />
+            <v-card v-if="!isMobile" :class="$round" elevation="10">
+              <achivement :achivement="todayPerson.achivement" />
+            </v-card>
           </v-col>
         </v-row>
       </v-col>
@@ -90,12 +71,9 @@ export default defineComponent({
 <style lang="scss" scoped>
 .person-info {
   .thumb {
-    transform: translateX(0%);
-    transition: all 0.8s ease;
-    &.on {
+    &.pc {
       position: sticky;
-      transform: translateX(-200%) translateY(30px);
-      top: 40px;
+      top: 58px;
     }
   }
 }
