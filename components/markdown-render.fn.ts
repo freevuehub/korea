827425785523e4915b41bc0263d75b47a887e5b1
freevue <hackish@hanmail.net>
2020-12-03 10:26:27 +0900

    [refactor]: 마드다운 렌더러 소스 정리

--- components/markdown-render.fn.ts
+++ components/markdown-render.fn.ts
@@ -4,30 +4,53 @@ interface IProps {
   markdown: string
 }
 
+interface IDomTree {
+  component: string
+  value?: string
+  tree?: IDomTree[]
+}
+
+const titleReg = /(#+)\s?(.+)/
+const dividerReg = /-{3}/
+const listReg = /-\s?(.+)/
+
+const contentsFilter = (string: string) => !!string
+const mdOfDom = (string: string) => {
+  if (titleReg.test(string)) {
+    const [, tag, value] = titleReg.exec(string) || ['', '', '']
+
+    return { component: `h${tag.length}`, value }
+  } else if (dividerReg.test(string)) {
+    return { component: 'hr' }
+  } else if (listReg.test(string)) {
+    const [, value] = listReg.exec(string) || ['', '']
+
+    return { component: 'li', value }
+  }
+
+  return { component: 'p', value: string }
+}
+const domReduce = (prev: IDomTree[], cur: IDomTree, index: number) => {
+  switch (cur.component) {
+    case 'li':
+      if (prev[index - 1].component === 'ul') {
+        prev[index - 1].tree?.push(cur)
+
+        return prev
+      }
+
+      return [...prev, { component: 'ul', tree: [cur] }]
+    case 'code':
+      return [...prev]
+    default:
+      return [...prev, cur]
+  }
+}
+
 export const useComputed = (props: IProps) => ({
   tree: computed(() => {
-    const titleReg = /(#+)\s?(.+)/
-    const dividerReg = /-{3}/
-    const listReg = /-\s?(.+)/
-    const contents = props.markdown
-      .split(/\n/)
-      .filter((string: string) => !!string)
-      .map((string: string) => {
-        if (titleReg.test(string)) {
-          const [, tag, value] = titleReg.exec(string) || ['', '', '']
+    const contents = props.markdown.split(/\n/)
 
-          return { component: `h${tag.length}`, value }
-        } else if (dividerReg.test(string)) {
-          return { component: 'hr' }
-        } else if (listReg.test(string)) {
-          const [, value] = listReg.exec(string) || ['', '']
-
-          return { component: 'li', value }
-        }
-
-        return { component: 'p', value: string }
-      })
-
-    return contents
+    return contents.filter(contentsFilter).map(mdOfDom).reduce(domReduce, [])
   }),
 })
