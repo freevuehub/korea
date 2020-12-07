be1a1f1c5854ba44b96f73e8bd303029a23b66c6
doo9392 <hackish@hanmail.net>
2020-12-07 19:51:56 +0900

    [new]: 역사적인 순간들 페이지 파일 추가

---
+++ pages/event.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container fluid>
+    event
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
