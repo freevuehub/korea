f47b1af27d7cc9fbf902cf5dd302b8f4497621cd
doo9392 <hackish@hanmail.net>
2020-12-11 10:56:29 +0900

    [improved]: 캘린더 구조 구성

---
+++ pages/event.fn.ts
@@ -0,0 +1,59 @@
+import { reactive, computed } from '@vue/composition-api'
+import dayjs from 'dayjs'
+
+interface IState {
+  weekday: number[]
+  value: string
+  events: any[]
+  colors: string[]
+  names: string[]
+}
+
+export const useState = () => reactive<IState>({
+  weekday: [0, 1, 2, 3, 4, 5, 6],
+  value: '2020-01-01',
+  events: [],
+  colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
+  names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
+})
+
+export const useComputed = (state: IState) => ({
+  nowMonth: computed(() => {
+    return dayjs(state.value, 'YYYY-MM-DD').format('YYYY-MM')
+  })
+})
+
+export const useGetEventColor = () => (event: any) => {
+  return event.color
+}
+
+export const useRnd = () => (a: number, b: number) => {
+  return Math.floor((b - a + 1) * Math.random()) + a
+}
+
+export const useGetEvents = (state: IState, rnd: any) => ({ start, end }: any) => {
+  const events = []
+
+  const min = new Date(`${start.date}T00:00:00`)
+  const max = new Date(`${end.date}T23:59:59`)
+  const days = (max.getTime() - min.getTime()) / 86400000
+  const eventCount = rnd(days, days + 20)
+
+  for (let i = 0; i < eventCount; i++) {
+    const allDay = rnd(0, 3) === 0
+    const firstTimestamp = rnd(min.getTime(), max.getTime())
+    const first = new Date(firstTimestamp - (firstTimestamp % 900000))
+    const secondTimestamp = rnd(2, allDay ? 288 : 8) * 900000
+    const second = new Date(first.getTime() + secondTimestamp)
+
+    events.push({
+      name: state.names[rnd(0, state.names.length - 1)],
+      start: first,
+      end: second,
+      color: state.colors[rnd(0, state.colors.length - 1)],
+      timed: !allDay,
+    })
+  }
+
+  state.events = events
+}
--- pages/event.vue
+++ pages/event.vue
@@ -2,7 +2,30 @@
   <v-container fluid>
     <v-card :class="$round">
       <v-card-text>
-        <v-calendar ref="calendar" type="month" />
+        <v-sheet class="d-flex mb-5">
+          <v-btn icon @click="$refs.calendar.prev()">
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+          <v-spacer />
+          <v-card-title class="pa-0">{{ nowMonth }}</v-card-title>
+          <v-spacer />
+          <v-btn icon @click="$refs.calendar.next()">
+            <v-icon>mdi-chevron-right</v-icon>
+          </v-btn>
+        </v-sheet>
+        <v-sheet height="600">
+          <v-calendar
+            ref="calendar"
+            v-model="state.value"
+            :weekdays="state.weekday"
+            type="month"
+            :events="state.events"
+            event-overlap-mode="stack"
+            :event-overlap-threshold="30"
+            :event-color="getEventColor"
+            @change="getEvents"
+          ></v-calendar>
+        </v-sheet>
       </v-card-text>
     </v-card>
   </v-container>
@@ -10,11 +33,24 @@
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
+import { useState, useComputed, useGetEventColor, useRnd, useGetEvents } from './event.fn'
 
 export default defineComponent({
   middleware: ['event'],
   setup() {
-    return {}
+    const state = useState()
+    const computed = useComputed(state)
+    const getEventColor = useGetEventColor()
+    const rnd = useRnd()
+    const getEvents = useGetEvents(state, rnd)
+
+    return {
+      state,
+      ...computed,
+      getEventColor,
+      rnd,
+      getEvents,
+    }
   },
 })
 </script>
